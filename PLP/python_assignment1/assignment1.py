2e53f4d5957254760895ed82a741ccf9c540f9e2
Dave5918 <omondidavve@gmail.com>
2025-03-14 23:30:24 +0300

    easy

---
+++ PLP/python_assignment1/assignment1.py
@@ -0,0 +1,13 @@
+x=int(input("enter value x "))
+y=int(input("enetr value y "))
+operation=input("enter operation(-,+,*,/) ")
+if operation =="+":
+    answer=x+y
+if operation =="-":
+    answer=x-y
+if operation =="*":
+    answer=x*y
+if operation =="/":
+    answer=x/y
+print(answer)
+
