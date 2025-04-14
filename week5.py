8bfbd30a6d448dcce4b338df256d827055215fb7
David Omondi <omondidavve@gmail.com>
2025-04-14 13:01:27 +0300

    Add files via upload

---
+++ week5.py
@@ -0,0 +1,9 @@
+class book:
+    def _init_(self,b_type,language):
+        self.b_type=b_type
+        self.language=language
+book1=("novel","english")
+book2=("play","spanish")
+
+print(book1.b_type)
+print(book2.language)
